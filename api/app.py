84305dcc646f76546d1aa8f3ba8ecda916f658ce
LATIFANIKA-NURAFWI1 <latifanika.nurafwi1@gmail.com>
2026-04-26 20:35:41 +0700

    prepare vercel

--- api/app.py
+++ api/app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, request
 import math
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    template_folder="../templates",
+    static_folder="../static",
+    static_url_path="/static"
+)
 app.secret_key = 'fuzzy_edu_v2_2025'
 app.jinja_env.globals.update(enumerate=enumerate, zip=zip)
 
